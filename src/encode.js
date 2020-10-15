ebcb1a79371c7e7bd451065412ce76721c060af6
Racso-3141 <enxuhan@gmail.com>
2020-10-15 14:49:11 +0800

    first commit

---
+++ src/encode.js
@@ -0,0 +1,117 @@
+let hexToBinary = require('hex-to-binary');
+
+//opcode dictionary-I; J. R type always has opcode 000000
+let op_dict = {
+    addi: "001000", addiu: "001001", andi: "001100", ori: "001101",  slti: "001010",
+    sltiu: "001011", xori: "001110", beq: "000100",  bne: "000101", lb: "100000", 
+    lbu: "100100", lw: "100011", sb:  "101000", sw:  "101011",  lui: "001111",
+    j: "000010", jal:  "000011"
+};
+
+// function code dictionary-R type
+let func_dict = {
+    add: "100000", addu: "100001", and: "100100", or: "100101", nor: "100111",
+    slt: "101010", sltu: "101011", sub: "100010", subu: "100011", div: "011010",
+    divu: "011011", mult: "011000", multu: "011001", xor: "100110", sll: "000000",
+    srl: "000010", jr: "001000"
+}
+
+
+export function encode_R_common(operation, rs_, rt_, rd_) {
+    const opcode = "000000";
+    let rs = (rs_ >>> 0).toString(2);
+    rs = "0".repeat(5 - rs.length) + rs;
+    let rt = (rt_ >>> 0).toString(2);
+    rt = "0".repeat(5 - rt.length) + rt;
+    let rd = (rd_ >>> 0).toString(2);
+    rd = "0".repeat(5 - rd.length) + rd;
+    const shamt = "00000"
+    const funct = func_dict[operation];
+    return opcode + rs  + rt  + rd + shamt + funct;
+}
+
+export function encode_R_shift(operation, rd_, rt_, shamt_) {
+    const opcode = "000000";
+    const rs = "00000";
+    let rt = (rt_ >>> 0).toString(2);
+    rt = "0".repeat(5 - rt.length) + rt;
+    let rd = (rd_ >>> 0).toString(2);
+    rd = "0".repeat(5 - rd.length) + rd;
+    let shamt = (shamt_ >>> 0).toString(2);
+    shamt = "0".repeat(5 - shamt.length) + shamt;
+    const funct = func_dict[operation];
+    return opcode + rs  + rt  + rd + shamt + funct;
+}
+
+export function encode_R_jr(operation, rs_) {
+    const opcode = "000000";
+    let rs = (rs_ >>> 0).toString(2);
+    rs = "0".repeat(5 - rs.length) + rs;
+    const rt = "00000";
+    const rd = "00000";
+    const shamt = "00000";
+    const funct = func_dict[operation];
+    return opcode + rs  + rt  + rd + shamt + funct;
+}
+
+export function encoder_I_common(operation, rt_, rs_, imm_) {
+    const opcode = op_dict[operation];
+    let rs = (rs_ >>> 0).toString(2);
+    rs = "0".repeat(5 - rs.length) + rs;
+    let rt = (rt_ >>> 0).toString(2);
+    rt = "0".repeat(5 - rt.length) + rt;
+    let imm = (imm_ >>> 0).toString(2);
+    imm = (imm_ < 0) ? (imm.slice(-16)): ("0".repeat(16 - imm.length) + imm);
+    return opcode + rs  + rt + imm;
+}
+
+export function encoder_I_bitwise(operation, rt_, rs_, hex_) {
+    const opcode = op_dict[operation];
+    let rs = (rs_ >>> 0).toString(2);
+    rs = "0".repeat(5 - rs.length) + rs;
+    let rt = (rt_ >>> 0).toString(2);
+    rt = "0".repeat(5 - rt.length) + rt;
+    let hex = hexToBinary(hex_);
+    hex = "0".repeat(16 - hex.length) + hex;
+    return opcode + rs  + rt + hex;
+}
+
+export function encoder_I_branch(operation, rs_, rt_, label_) {
+    const opcode = op_dict[operation];
+    let rs = (rs_ >>> 0).toString(2);
+    rs = "0".repeat(5 - rs.length) + rs;
+    let rt = (rt_ >>> 0).toString(2);
+    rt = "0".repeat(5 - rt.length) + rt;
+    let label = (label_ >>> 0).toString(2);
+    label = (label_ < 0) ? (label.slice(-16)): ("0".repeat(16 - label.length) + label);
+    return opcode + rs  + rt + label;
+}
+
+export function encoder_I_ls(operation, rt_, imm_, rs_) {
+    const opcode = op_dict[operation];
+    let rs = (rs_ >>> 0).toString(2);
+    rs = "0".repeat(5 - rs.length) + rs;
+    let rt = (rt_ >>> 0).toString(2);
+    rt = "0".repeat(5 - rt.length) + rt;
+    let imm = (imm_ >>> 0).toString(2);
+    imm = (imm_ < 0) ? (imm.slice(-16)): ("0".repeat(16 - imm.length) + imm);
+    return opcode + rs  + rt + imm;
+}
+
+export function encoder_I_lui(operation, rt_, imm_) {
+    const opcode = op_dict[operation];
+    let rs = "00000";
+    let rt = (rt_ >>> 0).toString(2);
+    rt = "0".repeat(5 - rt.length) + rt;
+    let imm = (imm_ >>> 0).toString(2);
+    imm = (imm_ < 0) ? (imm.slice(-16)): ("0".repeat(16 - imm.length) + imm);
+    return opcode + rs  + rt + imm;
+}
+
+export function encoder_J(operation, addr_) {
+    const opcode = op_dict[operation];
+    let addr = hexToBinary(addr_);
+    addr = "0".repeat(32 - addr.length) + addr;
+    addr = addr.slice(4, 30);
+    return opcode + addr;
+}
---
+++ src/index.js
@@ -0,0 +1,530 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as encode from './encode.js';
+
+class R_common extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "R_common", 
+      rs: "",
+      rt: "",
+      rd: "",
+      isSubmitted: false,
+      result: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({
+      isSubmitted: true, 
+      result: encode.encode_R_common(this.props.operation, this.state.rs, this.state.rt, this.state.rd)
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h2>R_common</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            $
+          <input type="text" name="rd" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+            $
+          <input type="text" name="rs" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+            $
+          <input type="text" name="rt" onChange={this.handleChange}/>
+          </label>
+          &nbsp;
+          <input type="submit" value="Encode" />
+        </form>
+        <h1>{this.state.isSubmitted && this.state.result}</h1>
+      </div>
+    );
+  }
+}
+
+class R_shift extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "R_shift", 
+      rd: "",
+      rt: "",
+      shamt: "",
+      isSubmitted: false,
+      result: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({
+      isSubmitted: true, 
+      result: encode.encode_R_shift(this.props.operation, this.state.rd, this.state.rt, this.state.shamt)
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h2>R_shift</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            $
+          <input type="text" name="rd" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+            $
+          <input type="text" name="rt" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+          <input type="text" name="shamt" onChange={this.handleChange}/>
+          </label>
+          &nbsp;
+          <input type="submit" value="Encode" />
+        </form>
+        <h1>{this.state.isSubmitted && this.state.result}</h1>
+      </div>
+    );
+  }
+}
+
+class R_jr extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "R_jr", 
+      rs: "",
+      isSubmitted: false,
+      result: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({
+      isSubmitted: true, 
+      result: encode.encode_R_jr(this.props.operation, this.state.rs)
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h2>R_jr</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            $
+          <input type="text" name="rs" onChange={this.handleChange}/>
+          </label>
+          &nbsp;
+          <input type="submit" value="Encode" />
+        </form>
+        <h1>{this.state.isSubmitted && this.state.result}</h1>
+      </div>
+    );
+  }
+}
+ 
+class I_common extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "I_common", 
+      rt: "",
+      rs: "",
+      imm: "",
+      isSubmitted: false,
+      result: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({
+      isSubmitted: true, 
+      result: encode.encoder_I_common(this.props.operation, this.state.rt, this.state.rs, this.state.imm)
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h2>I_common</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            $
+          <input type="text" name="rt" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+            $
+          <input type="text" name="rs" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+          <input type="text" name="imm" onChange={this.handleChange}/>
+          </label>
+          &nbsp;
+          <input type="submit" value="Encode" />
+        </form>
+        <h1>{this.state.isSubmitted && this.state.result}</h1>
+      </div>
+    );
+  }
+}
+
+class I_bitwise extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "I_bitwise", 
+      rt: "",
+      rs: "",
+      hex: "",
+      isSubmitted: false,
+      result: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({
+      isSubmitted: true, 
+      result: encode.encoder_I_bitwise(this.props.operation, this.state.rt, this.state.rs, this.state.hex)
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h2>I_bitwise</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            $
+          <input type="text" name="rt" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+            $
+          <input type="text" name="rs" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+            <strong>0x</strong>
+          <input type="text" name="hex" onChange={this.handleChange}/>
+          </label>
+          &nbsp;
+          <input type="submit" value="Encode" />
+        </form>
+        <h1>{this.state.isSubmitted && this.state.result}</h1>
+      </div>
+    );
+  }
+}
+
+class I_branch extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "I_branch",
+      rs: "",
+      rt: "",
+      label: "",
+      isSubmitted: false,
+      result: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({
+      isSubmitted: true, 
+      result: encode.encoder_I_branch(this.props.operation, this.state.rs, this.state.rt, this.state.label)
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h2>I_branch</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            $
+          <input type="text" name="rs" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+            $
+          <input type="text" name="rt" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+          <input type="text" name="label" onChange={this.handleChange}/>
+          </label>
+          &nbsp;
+          <input type="submit" value="Encode" />
+        </form>
+        <h1>{this.state.isSubmitted && this.state.result}</h1>
+      </div>
+    );
+  }
+}
+
+class I_ls extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "I_ls", 
+      rt: "",
+      imm: "",
+      rs: "",
+      isSubmitted: false,
+      result: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({
+      isSubmitted: true, 
+      result: encode.encoder_I_ls(this.props.operation, this.state.rt, this.state.imm, this.state.rs)
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h2>I_ls</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            $
+          <input type="text" name="rt" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+            (
+          <input type="text" name="imm" onChange={this.handleChange}/>
+            ) $
+          </label>
+          <label>
+          <input type="text" name="rs" onChange={this.handleChange}/>
+          </label>
+          &nbsp;
+          <input type="submit" value="Encode" />
+        </form>
+        <h1>{this.state.isSubmitted && this.state.result}</h1>
+      </div>
+    );
+  }
+}
+
+class I_lui extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "I_lui",
+      rt: "",
+      imm: "",
+      isSubmitted: false,
+      result: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({
+      isSubmitted: true, 
+      result: encode.encoder_I_ls(this.props.operation, this.state.rt, this.state.imm)
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h2>I_lui</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            $
+          <input type="text" name="rt" onChange={this.handleChange}/>
+          ，
+          </label>
+          <label>
+          <input type="text" name="imm" onChange={this.handleChange}/>
+          </label>
+          &nbsp;
+          <input type="submit" value="Encode" />
+        </form>
+        <h1>{this.state.isSubmitted && this.state.result}</h1>
+      </div>
+    );
+  }
+}
+
+class J extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "J",
+      label:"",
+      isSubmitted: false,
+      result: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event) {
+    this.setState({
+      isSubmitted: true, 
+      result: encode.encoder_J(this.props.operation, this.state.label)
+    });
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h2>J</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <strong>0x</strong>
+          <input type="text" name="label" onChange={this.handleChange}/>
+          </label>
+          &nbsp;
+          <input type="submit" value="Encode" />
+        </form>
+        <h1>{this.state.isSubmitted && this.state.result}</h1>
+      </div>
+    );
+  }
+}
+
+class Encode extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expression: "default",
+      operation: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      expression: event.target.value,
+      operation: event.nativeEvent.target[event.nativeEvent.target.selectedIndex].text
+    });
+  }
+  render() {
+    let expression = this.state.expression;
+    let operation = this.state.operation;
+    let ui;
+    if (expression === "default") {
+      ui = (<h1>Select a function above</h1>)
+    } else if (expression === "R_common") {
+      ui = <R_common operation = {operation}/>
+    } else if(expression === "R_shift") {
+      ui = <R_shift operation = {operation}/>
+    } else if(expression === "R_jr") {
+      ui = <R_jr operation = {operation}/>
+    } else if(expression === "I_common") {
+      ui = <I_common operation = {operation}/>
+    } else if(expression === "I_bitwise") {
+      ui = <I_bitwise operation = {operation}/>
+    } else if(expression === "I_branch") {
+      ui = <I_branch operation = {operation}/>
+    } else if(expression === "I_ls") {
+      ui = <I_ls operation = {operation}/>
+    } else if(expression === "I_lui") {
+      ui = <I_lui operation = {operation}/>
+    } else if(expression === "J") {
+      ui= <J operation = {operation}/>
+    }
+    return (
+      <div>
+        <form>
+          <label>
+            select the function name:
+            <select onChange={this.handleChange}>
+              <option>select</option>
+              <option value="R_common">add</option>
+              <option value="R_common">addu</option>
+              <option value="R_common">and</option>
+              <option value="R_common">or</option>
+              <option value="R_common">nor</option>
+              <option value="R_common">slt</option>
+              <option value="R_common">sltu</option>
+              <option value="R_common">sub</option>
+              <option value="R_common">subu</option>
+              <option value="R_common">div</option>
+              <option value="R_common">divu</option>
+              <option value="R_common">mult</option>
+              <option value="R_common">multu</option>
+              <option value="R_common">xor</option>
+              <option value="R_shift">sll</option>
+              <option value="R_shift">srl</option>
+              <option value="R_jr">jr</option>
+
+              <option value="I_common">addi</option>
+              <option value="I_common">addiu</option>
+              <option value="I_bitwise">andi</option>
+              <option value="I_bitwise">ori</option>
+              <option value="I_common">slti</option>
+              <option value="I_common">sltiu</option>
+              <option value="I_bitwise">xori</option>
+              <option value="I_branch">beq</option>
+              <option value="I_branch">bne</option>
+              <option value="I_ls">lb</option>
+              <option value="I_ls">lbu</option>
+              <option value="I_ls">lw</option>
+              <option value="I_ls">sb</option>
+              <option value="I_ls">sw</option>
+              <option value="I_lui">lui</option>
+
+              <option value="J">j</option>
+              <option value="J">jal</option>
+            </select>
+          </label>
+        </form>
+        {ui}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Encode />, document.getElementById('root'));
